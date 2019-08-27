7d75ec90ac5fbfcac099415a90402810bb724617
Benjamin Shields <ben.d.shields@gmail.com>
2019-08-27 12:48:28 -0700

    Separate readWriteCloser from blockStreamer
    The tftp.client will use the readWriteCloser interface as a handler,
    with a first implementation as struct type blockStreamer.
    
    Define the purpose of blockStreamer to more closely match its use in
    TFTP.
    
    Add openFlag & encodingFlag types to control behavior of blockStreamer.

--- filehandler.go
+++ filehandler.go
@@ -5,35 +5,74 @@
 // Package tftp2 implements the Trivial File Transfer Protocol as defined in RFC 1350.
 package tftp2
 
-import "os"
+import (
+	"bufio"
+	"io"
+	"os"
+)
 
-type fileHandler struct {
-	filename      string // TODO LATER maybe make this an *os.File?
-	mode          string
-	blockSize     int64
-	fileReference os.File
+// readWriteCloser is the type that must be implemented by the handler of the client type.
+type readWriteCloser io.ReadWriteCloser
+
+// openFlag controls behavior of opening a file with a blockStreamer.
+type openFlag int
+
+const (
+	// read is a flag to open a file in read-only mode.
+	read openFlag = openFlag(os.O_RDONLY)
+
+	// write is a flag to open a file in write-only append mode, creating it if it does not exist.
+	write openFlag = openFlag(os.O_CREATE | os.O_APPEND | os.O_WRONLY)
+)
+
+// encodingFlag controls encoding / decoding behavior of writing and reading to a file with a blockStreamer.
+type encodingFlag int
+
+const (
+	// netascii is a flag to handle data according to netascii as described by RFC 764.
+	netascii encodingFlag = iota
+
+	// octet is a flag to handle data as-is in 8-bit binary form.
+	octet
+)
+
+// blockStreamer provides an efficient interface for streaming small,
+// block-sized read-only or write-only file operations together.
+type blockStreamer struct {
+	filename string       // filename is used to open the underlying operating system file.
+	openMode openFlag     // openMode controls which type of I/O operation will be streamed; read-only or write-only.
+	encoding encodingFlag // encoding controls whether data will be streamed as netascii or not.
+
+	fileReference *os.File          // fileReference will be opened and closed by the Open() & Close() calls to a blockStreamer.
+	buffer        *bufio.ReadWriter // buffer serves as the intermediary reader or writer to the fileReference.
 }
 
-func NewFileHandler(filename, mode string, blockSize int64) *fileHandler {
-	return &fileHandler{filename, mode, blockSize, nil}
+func newBlockStreamer(filename string, openFlag openFlag, encFlag encodingFlag) *blockStreamer {
+	fh := blockStreamer{
+		filename,
+		openFlag,
+		encFlag,
+		nil,
+		nil}
+	return &fh
 }
 
-func (fh fileHandler) open() error {
-	/* TODO: Implement func (fh fileHandler) open(filename, mode string) error */
+func (fh blockStreamer) Open() error {
+	/* TODO: Implement func (fh blockStreamer) open(filename, mode string) error */
 	return nil
 }
 
-func (fh fileHandler) close() error {
-	/* TODO: Implement func (fh fileHandler) close() error */
+func (fh blockStreamer) Close() error {
+	/* TODO: Implement func (fh blockStreamer) close() error */
 	return nil
 }
 
-func (fh fileHandler) read() ([]byte, error) {
-	/* TODO: Implement func (fh fileHandler) read() ([]byte, error) */
-	return nil, nil
+func (fh blockStreamer) Read(b []byte) (n int, err error) {
+	/* TODO: Implement func (fh blockStreamer) read() ([]byte, error) */
+	return 0, nil
 }
 
-func (fh fileHandler) write(data []byte) error {
-	/* TODO: Implement func (fh fileHandler) write(data []byte) error */
-	return nil
+func (fh blockStreamer) Write(b []byte) (n int, err error) {
+	/* TODO: Implement func (fh blockStreamer) write(data []byte) error */
+	return 0, nil
 }
