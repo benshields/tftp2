190c0eb3a69174703779c644a62799d2d81ef35d
Benjamin Shields <ben.d.shields@gmail.com>
2019-08-28 18:23:14 -0700

    Define roles for types: server, conn, client
    Not in a testable state, needs further implementation. Check the FIXME
    comment to find where implementation left off.
    
    Filled out more implementation for the server and introduced a type
    conn. The server uses a conn to accept incoming packets at :tftp. It
    then creates new conns at a unique ephemeral port to serve each client.

--- client.go
+++ client.go
@@ -5,16 +5,52 @@
 // Package tftp2 implements the Trivial File Transfer Protocol as defined in RFC 1350.
 package tftp2
 
-import "net"
+import (
+	"net"
+	"strconv"
+)
 
 // client defines parameters for maintaining a TFTP client connection.
 type client struct {
-	addr net.UDPAddr // addr is the UDP address at which this client can be reached.
-
-	conn net.UDPConn // conn is the server-assigned connection to serve the client from.
+	conn // the server-assigned connection to serve the client from.
 
 	/* TODO: change this into a tftp.packet */
 	lastPacket []byte // lastPacket holds the last packet received from the client in case of re-transmission.
 
 	handler readWriteCloser // handler interfaces with the file that the client is reading from or writing to.
+
+	localTID uint16 // this is the port of localAddr which is the server-side Transfer ID in TFTP
+
+	remoteAddr net.Addr // address at which this client can be reached
+
+	remoteTID uint16 // this is the port of remoteAddr which is the client-side Transfer ID in TFTP
+}
+
+// FIXME: This function got ripped out from type *conn, so adapt it to take addresses
+// Create a new client for this connection and assign the endpoint ports as the Transfer IDs.
+func (c *client) assignTID() error {
+	// the local port serves as the connections local TID
+	c.localAddr = conn.LocalAddr()
+	_, localTIDstr, err := net.SplitHostPort(c.localAddr.String())
+	if err != nil {
+		return err
+	}
+	localTID, err := strconv.Atoi(localTIDstr)
+	if err != nil {
+		return err
+	}
+	c.localTID = uint16(localTID)
+
+	// the remote port serves as the connections remote TID
+	_, remoteTIDstr, err := net.SplitHostPort(c.remoteAddr.String())
+	if err != nil {
+		return err
+	}
+	remoteTID, err := strconv.Atoi(remoteTIDstr)
+	if err != nil {
+		return err
+	}
+	c.remoteTID = uint16(remoteTID)
+
+	return nil
 }
--- server.go
+++ server.go
@@ -8,6 +8,7 @@ package tftp2
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log"
 	"net"
 	"os"
@@ -69,7 +70,7 @@ func (srv *Server) ListenAndServe() error {
 	if addr == "" {
 		addr = ":tftp"
 	}
-	conn, err := net.ListenPacket("udp", addr)
+	conn, err := srv.newConn(addr)
 	if err != nil {
 		return err
 	}
@@ -87,43 +88,25 @@ func (srv *Server) Close() error {
 //
 // Serve always returns a non-nil error and closes conn.
 // After Shutdown or Close, the returned error is ErrServerClosed.
-func (srv *Server) serve(conn net.PacketConn) error {
-	/* TODO: Implement func (srv *Server) serve(conn net.PacketConn) error */
-	conn = &onceClosePacketConn{PacketConn: conn}
-	defer conn.Close()
+func (srv *Server) serve(tftpConn *conn) error {
+	baseCtx := context.Background()                          // base is always background, per Issue 16220
+	ctx := context.WithValue(baseCtx, ServerContextKey, srv) // TODO: Design - Do I really need this context value or Context keys?
+	///////////
 
-	var tempDelay time.Duration     // how long to sleep on accept failure
-	baseCtx := context.Background() // base is always background, per Issue 16220
-	ctx := context.WithValue(baseCtx, ServerContextKey, srv)
-	buffer := make([]byte, bufferSize)
+	tftpConn.rwc = &onceClosePacketConn{PacketConn: tftpConn.rwc}
+	defer tftpConn.rwc.Close() /* TODO: Later - How do handle the error of a deferred function? */
 	for {
-		n, addr, err := conn.ReadFrom(buffer)
+		request, clientAddr, err := tftpConn.readWithRetry(ctx)
 		if err != nil {
-			select {
-			case <-srv.getDoneChan():
-				return ErrServerClosed
-			default:
-			}
-			if ne, ok := err.(net.Error); ok && ne.Temporary() {
-				if tempDelay == 0 {
-					tempDelay = 5 * time.Millisecond
-				} else {
-					tempDelay *= 2
-				}
-				if max := 1 * time.Second; tempDelay > max {
-					tempDelay = max
-				}
-				srv.logf("tftp: Request error: %v; retrying in %v", err, tempDelay)
-				time.Sleep(tempDelay)
-				continue
-			}
 			return err
 		}
-		tempDelay = 0
-		// FIXME This is where I'm leaving off for the night. This is where I'll setup a client connection and serve it.
-		c := srv.newConn(rw)
-		c.setState(c.rwc, StateNew) // before Serve can return
-		go c.serve(ctx)
+
+		// :0 assigns a new ephemeral port from the OS, this becomes the server-side Transfer ID (TID) of the connection
+		clientConn, err := srv.newConn("127.0.0.1:0")
+		if err != nil {
+			return err
+		}
+		go clientConn.serve(ctx, clientAddr, request)
 	}
 }
 
@@ -198,10 +181,206 @@ func (srv *Server) closeDoneChanLocked() {
 	}
 }
 
-func (s *Server) logf(format string, args ...interface{}) {
-	if s.ErrorLog != nil {
-		s.ErrorLog.Printf(format, args...)
+func (srv *Server) logf(format string, args ...interface{}) {
+	if srv.ErrorLog != nil {
+		srv.ErrorLog.Printf(format, args...)
 	} else {
 		log.Printf(format, args...)
 	}
+}
+
+//////////////////////////////////////////////////
+
+// A conn represents the server side of an TFTP connection.
+type conn struct {
+	// server is the server on which the connection arrived.
+	// Immutable; never nil.
+	server *Server
+
+	// cancelCtx cancels the connection-level context.
+	cancelCtx context.CancelFunc
+
+	// rwc is the underlying network connection.
+	// This is never wrapped by other types and is the value given out
+	// to CloseNotifier callers.
+	rwc net.PacketConn
+
+	buffer []byte // buffer holds the data last read from rwc
+
+	localAddr net.Addr // address from which the handler is serving the connection
+}
+
+// Create new connection from addr.
+func (srv *Server) newConn(addr string) (*conn, error) {
+	pc, err := net.ListenPacket("udp", addr)
+	if err != nil {
+		return nil, err
+	}
+	c := &conn{
+		server:    srv,
+		rwc:       pc,
+		buffer:    make([]byte, bufferSize),
+		localAddr: pc.LocalAddr(),
+	}
+	return c, nil
+}
+
+func (c *conn) readWithRetry(ctx context.Context) (data []byte, addr net.Addr, err error) {
+	var tempDelay time.Duration // how long to sleep on accept failure
+	for {
+		n, addr, err := c.rwc.ReadFrom(c.buffer)
+		if err != nil {
+			select {
+			case <-c.server.getDoneChan():
+				data := make([]byte, n)
+				copy(data, c.buffer[:n])
+				return data, addr, ErrServerClosed
+			default:
+			}
+			if ne, ok := err.(net.Error); ok && ne.Temporary() {
+				if tempDelay == 0 {
+					tempDelay = 5 * time.Millisecond
+				} else {
+					tempDelay *= 2
+				}
+				if max := 1 * time.Second; tempDelay > max {
+					tempDelay = max
+				}
+				c.server.logf("tftp: Request error: %v; retrying in %v", err, tempDelay)
+				time.Sleep(tempDelay)
+				continue // retry read
+			}
+			data := make([]byte, n)
+			copy(data, c.buffer[:n])
+			return data, addr, err // read failure
+		}
+		data := make([]byte, n)
+		copy(data, c.buffer[:n])
+		return data, addr, nil // read success
+	}
+}
+
+func (c *conn) serve(ctx context.Context, clientAddr net.Addr, req []byte) {
+	// http Server's CODE
+	ctx, cancelCtx := context.WithCancel(ctx)
+	c.cancelCtx = cancelCtx
+	defer cancelCtx()
+	// http Server's CODE
+
+	// Ben's CODE
+	/* FIXME: This should become something like:
+	- make a new client struct
+	- keep sending packets from this connection to the client's handler in a loop
+	*/
+	if err := clientConn.assignTID(); err != nil {
+		srv.logf("tftp: Assign TID to connection at %v resulted in error: %v", clientConn.remoteAddr, err)
+	}
+
+	buffer := make([]byte, bufferSize)
+	for {
+		n, addr, err := c.rwc.ReadFrom(buffer)
+		if err != nil {
+			c.server.logf("tftp: Serve connection error on read from packet connection:\n\tclientTID: %v\n\tserverTID: %v\n\terror: %v",
+				c.remoteTID, c.localTID, err)
+
+		}
+
+		// go s.Handler.ServeTFTP(r ResponseWriter, *Request) // TODO 3
+		go s.Handler.ServeTFTP(s.pc, &Request{s.clients, addr, buffer[:n]})
+	}
+	// Ben's CODE
+
+	// http Server's CODE
+	for {
+		w, err := c.readRequest(ctx)
+		if c.r.remain != c.server.initialReadLimitSize() {
+			// If we read any bytes off the wire, we're active.
+			c.setState(c.rwc, StateActive)
+		}
+		if err != nil {
+			const errorHeaders = "\r\nContent-Type: text/plain; charset=utf-8\r\nConnection: close\r\n\r\n"
+
+			if err == errTooLarge {
+				// Their HTTP client may or may not be
+				// able to read this if we're
+				// responding to them and hanging up
+				// while they're still writing their
+				// request. Undefined behavior.
+				const publicErr = "431 Request Header Fields Too Large"
+				fmt.Fprintf(c.rwc, "HTTP/1.1 "+publicErr+errorHeaders+publicErr)
+				c.closeWriteAndWait()
+				return
+			}
+			if isCommonNetReadError(err) {
+				return // don't reply
+			}
+
+			publicErr := "400 Bad Request"
+			if v, ok := err.(badRequestError); ok {
+				publicErr = publicErr + ": " + string(v)
+			}
+
+			fmt.Fprintf(c.rwc, "HTTP/1.1 "+publicErr+errorHeaders+publicErr)
+			return
+		}
+
+		// Expect 100 Continue support
+		req := w.req
+		if req.expectsContinue() {
+			if req.ProtoAtLeast(1, 1) && req.ContentLength != 0 {
+				// Wrap the Body reader with one that replies on the connection
+				req.Body = &expectContinueReader{readCloser: req.Body, resp: w}
+			}
+		} else if req.Header.get("Expect") != "" {
+			w.sendExpectationFailed()
+			return
+		}
+
+		c.curReq.Store(w)
+
+		if requestBodyRemains(req.Body) {
+			registerOnHitEOF(req.Body, w.conn.r.startBackgroundRead)
+		} else {
+			w.conn.r.startBackgroundRead()
+		}
+
+		// HTTP cannot have multiple simultaneous active requests.[*]
+		// Until the server replies to this request, it can't read another,
+		// so we might as well run the handler in this goroutine.
+		// [*] Not strictly true: HTTP pipelining. We could let them all process
+		// in parallel even if their responses need to be serialized.
+		// But we're not going to implement HTTP pipelining because it
+		// was never deployed in the wild and the answer is HTTP/2.
+		serverHandler{c.server}.ServeHTTP(w, w.req)
+		w.cancelCtx()
+		if c.hijacked() {
+			return
+		}
+		w.finishRequest()
+		if !w.shouldReuseConnection() {
+			if w.requestBodyLimitHit || w.closedRequestBodyEarly() {
+				c.closeWriteAndWait()
+			}
+			return
+		}
+		c.setState(c.rwc, StateIdle)
+		c.curReq.Store((*response)(nil))
+
+		if !w.conn.server.doKeepAlives() {
+			// We're in shutdown mode. We might've replied
+			// to the user without "Connection: close" and
+			// they might think they can send another
+			// request, but such is life with HTTP/1.1.
+			return
+		}
+
+		if d := c.server.idleTimeout(); d != 0 {
+			c.rwc.SetReadDeadline(time.Now().Add(d))
+			if _, err := c.bufr.Peek(4); err != nil {
+				return
+			}
+		}
+		c.rwc.SetReadDeadline(time.Time{})
+	}
+	// http Server's CODE
 }
